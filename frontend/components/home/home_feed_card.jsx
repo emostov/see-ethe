352b5f2adbc5c309a24ba2ee679bf2b2cb6094e2
zeke <emostov@middlebury.edu>
2020-03-05 13:51:57 -0800

    implement age updates every second

--- frontend/components/home/home_feed_card.jsx
+++ frontend/components/home/home_feed_card.jsx
@@ -16,7 +16,8 @@ import {
 import BlockItem from './block_item'
 
 
-
+// utility functions
+// TODO move these to a utility file
 const minutesAndSeconds = (nSeconds) => {
   const minutes = Math.floor(nSeconds / 60);
   const seconds = nSeconds - (minutes * 60);
@@ -38,56 +39,77 @@ const itemAgeToString = (item) => {
   return minutesAndSeconds(diff)
 }
 
-const HomeFeedCard = (props) => {
+export default class HomeFeedCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      intervalID: 0,
+      count: 0,
+    }
 
-  const mapItems = () => {
-    return props.items.map((item) => {
+    this.setState = this.setState.bind(this);
+  }
 
-      // const diff = calculateTimeDiff(b);
+  componentDidMount() {
 
+    const intervalID = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }, 1000)
+
+    this.setState({ intervalID });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalID)
+  }
+
+  mapItems() {  //TODO modify for Txns
+    return this.props.items.map((item) => {
       const age = itemAgeToString(item)
-      console.log(age)
+
       // ternary goes here for block / txn items
       return <BlockItem block={item} key={item.hash} age={age} />
-
     });
 
   }
-  const itemsComponents = () => {
-    if (props.items === null) { return '' }
-    return props.feedType === 'Blocks' ? (
-      mapItems()
+
+  itemsComponents() { //TODO modify for Txns
+    const { items, feedType } = this.props;
+    if (items === null) { return '' }
+    return feedType === 'Blocks' ? (
+      this.mapItems()
 
     ) : (
         ""
       )
   }
 
-  // console.log('time diff', calculateTimeDiff())
+  render() {
+    const { feedType } = this.props
+    return (
+      <Card className='h-100'>
+        <CardHeader tag="h3" className='card-header-title'>
+          Latest {feedType}
+        </CardHeader>
+        <CardBody className='card-scroll-holder'>
 
+          <div className='scroll-container'>
+            <div className='scroll-items-container'>
+              {this.itemsComponents()}
 
-  return (
-    <Card className='h-100'>
-      <CardHeader tag="h3" className='card-header-title'>
-        Latest {props.feedType}
-      </CardHeader>
-      <CardBody className='card-scroll-holder'>
-
-        <div className='scroll-container'>
-          <div className='scroll-items-container'>
-            {itemsComponents()}
-
+            </div>
           </div>
-        </div>
 
-      </CardBody>
-      <CardFooter className="">
-        <Button tag='a' className='feed-footer-btn w-100'>
-          View all {props.feedType.toLowerCase()}
-        </Button>
-      </CardFooter>
-    </Card>
-  )
+        </CardBody>
+        <CardFooter className="">
+          <Button tag='a' className='feed-footer-btn w-100'>
+            View all {feedType.toLowerCase()}
+          </Button>
+        </CardFooter>
+      </Card>
+    )
+  }
 }
 
-export default HomeFeedCard;
