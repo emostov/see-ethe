9a2e6e649a57947798485debfe7e68ee653de838
zeke <emostov@middlebury.edu>
2020-03-09 20:59:17 -0700

    put links on all feed items to respective block

--- frontend/components/block/block_page_route_container.js
+++ frontend/components/block/block_page_route_container.js
@@ -1,11 +1,8 @@
 import { connect } from 'react-redux';
 
 import BlockPage from './block_page'
-import { selectNMostRecentBlocksArray } from '../../reducers/selectors';
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(ownProps.match.params.hash);
-  // console.log(ownProps.match.hash);
   return {
     block: state.entities.blocks[ownProps.match.params.hash],
     addressTypeTags: state.entities.addressTypeTags,
--- frontend/components/home/transaction_item.jsx
+++ frontend/components/home/transaction_item.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col, Media, } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 import { sliceToDisplayAddress } from '../../util/general_util';
 import { web3 } from '../../util/web3_util'
@@ -7,7 +8,7 @@ import { web3 } from '../../util/web3_util'
 const convertWeiToShowValue = (wei) => web3.utils.fromWei(wei, 'ether').slice(0, 8)
 
 const TransactionItem = ({ tx, age }) => {
-  if (!tx) return(
+  if (!tx) return (
     <div> </div>
   )
   const { hash } = tx;
@@ -24,7 +25,10 @@ const TransactionItem = ({ tx, age }) => {
               </span>
             </div>
             <Media body className='feed ml-1'>
-              <a className='feed ml-1'>{sliceToDisplayAddress(hash)}</a>
+              {/* <a className='feed ml-1'>{sliceToDisplayAddress(hash)}</a> */}
+
+              <Link className='feed ml-1'
+                to={`/block/${tx.blockHash}`}>{sliceToDisplayAddress(hash)}</Link>
               <span
                 className='d-sm-block txt-2 ml-1 ml-sm-9 text-nowrap secondary-small'>
                 {age} {/* time elapsed  */}
@@ -37,10 +41,14 @@ const TransactionItem = ({ tx, age }) => {
           <div className='d-flex justify-content-between'>
             <div className='text-nowrap w-max-100'>
               <span className='d-block mb-1 mb-sm-0 norm-txt'>
-                From: <a className='text-truncate feed name'>{sliceToDisplayAddress(tx.from)}</a>
+                From: <Link className='text-truncate feed name'
+                  to={`/block/${tx.blockHash}`}>{sliceToDisplayAddress(tx.from)}</Link>
+                  {/* <a className='text-truncate feed name'>{sliceToDisplayAddress(tx.from)}</a> */}
               </span>
               <span className='d-sm-block norm-txt'>
-                To: <a className='text-truncate feed name'>{sliceToDisplayAddress(tx.to)}</a>
+                To: <Link className='text-truncate feed name'
+                  to={`/block/${tx.blockHash}`}>{sliceToDisplayAddress(tx.to)}</Link>
+                {/* <a className='text-truncate feed name'>{sliceToDisplayAddress(tx.to)}</a> */}
               </span>
             </div>
 
