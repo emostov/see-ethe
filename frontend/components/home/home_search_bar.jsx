c2b9bdf8b33857290d544752df6e68efe3621829
zeke <emostov@middlebury.edu>
2020-03-10 11:38:08 -0700

    search working

--- frontend/components/home/home_search_bar.jsx
+++ frontend/components/home/home_search_bar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import ReactDOM from "react-dom"
-import { useHistory } from "react-router-dom";
+import { Redirect } from 'react-router'
+
 import {
   Container,
   Card,
@@ -16,21 +16,21 @@ import {
 } from 'reactstrap';
 
 const HomeSearchBar = ({ blocks }) => {
+  let blockURL = '/block'
   const [splitButtonOpen, setSplitButtonOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState('')
   const [visible, setVisible] = useState(false);
+  const [redirectToBlock, setRedirectToBlock] = useState(false)
   const onDismiss = () => setVisible(false);
 
   const toggleSplit = () => setSplitButtonOpen(!splitButtonOpen);
-
+  const toggleRedirect = () => setRedirectToBlock(!redirectToBlock);
   const updateSearchTerm = (e) => setSearchTerm(e.currentTarget.value);
 
   const redirectToBlockPage = (hash) => {
-    const history = useHistory();
-    // console.log(hash)
-    // console.log('block')
-    console.log(`/block/${hash}`)
-    // history.push(path);
+    blockURL = (`/block/${hash}`);
+    // blockURL is used in render with redirect
+    toggleRedirect();
   }
 
   const searchBlock = () => {
@@ -54,58 +54,61 @@ const HomeSearchBar = ({ blocks }) => {
     setVisible(true);
   }
 
-  // redirectToBlockPage('helllllloooooo')
-
-  return (
-    <Container fluid="lg" className='md-4'>
-      <div className='main-contents'>
-        <Card body className='gradient-half-primary-body' >
-          <div className='search-header'>
-            <h5 className='white-text'>
-              Ethereum Block Chain Explorer
+  return redirectToBlock ?
+    (
+      <Redirect to={blockURL} />
+    )
+    :
+    (
+      <Container fluid="lg" className='md-4'>
+        <div className='main-contents'>
+          <Card body className='gradient-half-primary-body' >
+            <div className='search-header'>
+              <h5 className='white-text'>
+                Ethereum Block Chain Explorer
               </h5>
-            <div className='quick-links white-text'>
-              <ul className='small-text'>
-                <li className='small-text'>Quick links: </li>
-                <li className='small-text dt'>ERC-20 Quick Links</li>
-                <li className='small-text dt'>ERC-721 Quick Links</li>
-              </ul>
+              <div className='quick-links white-text'>
+                <ul className='small-text'>
+                  <li className='small-text'>Quick links: </li>
+                  <li className='small-text dt'>ERC-20 Quick Links</li>
+                  <li className='small-text dt'>ERC-721 Quick Links</li>
+                </ul>
+              </div>
             </div>
-          </div>
-          <InputGroup>
-            <InputGroupButtonDropdown addonType="prepend"
-              isOpen={splitButtonOpen} toggle={toggleSplit}>
-              <DropdownToggle split className='white-btn mid-search-txt' >All Filters  </DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem className='nf' disabled>Comming soon...</DropdownItem>
-                <DropdownItem className='nf' disabled>...</DropdownItem>
-                <DropdownItem className='nf' disabled>...</DropdownItem>
-                <DropdownItem className='nf' disabled>...</DropdownItem>
-                <DropdownItem className='nf' disabled>...</DropdownItem>
+            <InputGroup>
+              <InputGroupButtonDropdown addonType="prepend"
+                isOpen={splitButtonOpen} toggle={toggleSplit}>
+                <DropdownToggle split className='white-btn mid-search-txt' >All Filters  </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem className='nf' disabled>Comming soon...</DropdownItem>
+                  <DropdownItem className='nf' disabled>...</DropdownItem>
+                  <DropdownItem className='nf' disabled>...</DropdownItem>
+                  <DropdownItem className='nf' disabled>...</DropdownItem>
+                  <DropdownItem className='nf' disabled>...</DropdownItem>
 
-              </DropdownMenu>
-            </InputGroupButtonDropdown >
-            <Input
-              className='mid-search-txt'
-              placeholder="Search by block height or hash (use the feed for block info)"
-              value={searchTerm}
-              onChange={updateSearchTerm}
-            />
-            <InputGroupAddon addonType="append">
-              <Button
-                className='search-btn'
-                color="secondary"
-                onClick={searchBlock}
-              >Search</Button>
-            </InputGroupAddon>
-          </InputGroup>
-        </Card>
-      </div>
-      <Alert color="info" isOpen={visible} toggle={onDismiss}>
-        Sorry, we could not find a block using the provided search term. Search ability is limited to exact matches of blocks in the below feed.
+                </DropdownMenu>
+              </InputGroupButtonDropdown >
+              <Input
+                className='mid-search-txt'
+                placeholder="Search by block height or hash (use the feed for block info)"
+                value={searchTerm}
+                onChange={updateSearchTerm}
+              />
+              <InputGroupAddon addonType="append">
+                <Button
+                  className='search-btn'
+                  color="secondary"
+                  onClick={searchBlock}
+                >Search</Button>
+              </InputGroupAddon>
+            </InputGroup>
+          </Card>
+        </div>
+        <Alert color="info" isOpen={visible} toggle={onDismiss}>
+          Sorry, we could not find a block using the provided search term. Search ability is limited to exact matches of blocks in the below feed.
     </Alert>
-    </Container>
-  )
+      </Container>
+    )
 }
 
 export default HomeSearchBar;
