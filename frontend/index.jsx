ff79847ddfb4af2098a085a70364913a46f4c447
zeke <emostov@middlebury.edu>
2020-03-10 17:49:35 -0700

    work on ether wrap contract interaction

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import configureStore from './store/store'
 import Root from './components/root'
 import * as Web3Util from './util/web3_util';
+import EtherWrap from './contract/ether_wrap';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -22,6 +23,16 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore();
   }
 
- 
+
   ReactDOM.render(<Root store={store} />, root);
+
+  // console.log(EtherWrap.options.address)
+  // console.log(EtherWrap)
+  console.log(EtherWrap._jsonInterface)
+  console.log(EtherWrap.methods)
+  EtherWrap
+    .methods
+    .name()
+    .call()
+    .then(console.log);
 });
